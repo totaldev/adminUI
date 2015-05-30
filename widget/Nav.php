60a65075c1edc9e73108366731f090cfa1bebf59
Vadim Kovalenko <4ws@mail.ru>
2015-05-30 20:44:10 +0300

    check nav active item

--- widget/Nav.php
+++ widget/Nav.php
@@ -155,4 +155,37 @@ class Nav extends \yii\bootstrap\Nav {
         }
         return $items;
     }
+
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function isItemActive($item) {
+		if (isset($item['url']) && is_array($item['url']) && isset($item['url'][0])) {
+			$route = $item['url'][0];
+			if ($route[0] !== '/' && Yii::$app->controller) {
+				$route = Yii::$app->controller->module->getUniqueId().'/'.$route;
+			}
+
+			$route = ltrim($route, '/');
+			if (!$route || !substr_count(ltrim($this->route, '/'), $route)) {
+				return false;
+			}
+
+			unset($item['url']['#']);
+			if (count($item['url']) > 1) {
+				$params = $item['url'];
+				unset($params[0]);
+				foreach ($params as $name => $value) {
+					if ($value !== null && (!isset($this->params[$name]) || $this->params[$name] != $value)) {
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		return false;
+	}
 }
